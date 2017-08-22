a672c518f9113debda01f2456e8837334143d7f8
RenePonto <rene.ponto@paluno.uni-due.de>
2017-08-22 16:48:03 +0200

    commented non compiling code out

--- src/main/java/hu/mta/sztaki/lpds/cloud/simulator/iaas/vmallocmultitenant/MultiTenantConsolidator.java
+++ src/main/java/hu/mta/sztaki/lpds/cloud/simulator/iaas/vmallocmultitenant/MultiTenantConsolidator.java
@@ -16,58 +16,58 @@ public class MultiTenantConsolidator extends Consolidator{
 
 	@Override
 	protected void doConsolidation(PhysicalMachine[] pmList) {
-		try {
-			reoptimize(pmList);
-		} catch (VMManagementException e) {
-			e.printStackTrace();
-		} catch (NetworkException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void reoptimize(PhysicalMachine[] pms) throws VMManagementException, NetworkException {
-		// first, check if the number of active pms can be minimized
-		for(PhysicalMachine actualPm : pms) {
-			for(int i = 0; i < pms[i].publicVms.size(); i++) {
-				// migrate vm to first fit pm
-			}
-			
-			if(actualPm.publicVms.size() == 0) {
-				// commit the tentative migrations
-				actualPm.switchoff(null);
-			}
-			else {
-				// undo migrations
-			}
-		}
-
-		// check if a secure pm can take load from two (non-secure) pms
-		while(/*bed*/) {
-			PhysicalMachine pmBoth;
-			pmBoth.turnon();
-			
-			//migrate all vms of unsecure pm1
-			for(int i = 0; i < pm1.publicVms.size(); i++) {
-				//migrate
-			}
-			pm1.switchoff(null);
-			
-			//migrate all vms of unsecure pm2
-			for(int j = 0; j < pm2.publicVms.size(); 2++) {
-				//migrate
-			}
-			pm2.switchoff(null);
-		}
-
-		// check if the load of a secure pm can be moved to a non-secure pm
-		while(/*bed*/) {
-			PhysicalMachine unsecurePm;
-			unsecurePm.turnon();
-			for(int i = 0; i < securePm.publicVms.size(); i++) {
-				//migrate
-			}
-			securePm.switchOff(null);
-		}		
+//		try {
+//			reoptimize(pmList);
+//		} catch (VMManagementException e) {
+//			e.printStackTrace();
+//		} catch (NetworkException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	private void reoptimize(PhysicalMachine[] pms) throws VMManagementException, NetworkException {
+//		// first, check if the number of active pms can be minimized
+//		for(PhysicalMachine actualPm : pms) {
+//			for(int i = 0; i < pms[i].publicVms.size(); i++) {
+//				// migrate vm to first fit pm
+//			}
+//			
+//			if(actualPm.publicVms.size() == 0) {
+//				// commit the tentative migrations
+//				actualPm.switchoff(null);
+//			}
+//			else {
+//				// undo migrations
+//			}
+//		}
+//
+//		// check if a secure pm can take load from two (non-secure) pms
+//		while(/*bed*/) {
+//			PhysicalMachine pmBoth;
+//			pmBoth.turnon();
+//			
+//			//migrate all vms of unsecure pm1
+//			for(int i = 0; i < pm1.publicVms.size(); i++) {
+//				//migrate
+//			}
+//			pm1.switchoff(null);
+//			
+//			//migrate all vms of unsecure pm2
+//			for(int j = 0; j < pm2.publicVms.size(); 2++) {
+//				//migrate
+//			}
+//			pm2.switchoff(null);
+//		}
+//
+//		// check if the load of a secure pm can be moved to a non-secure pm
+//		while(/*bed*/) {
+//			PhysicalMachine unsecurePm;
+//			unsecurePm.turnon();
+//			for(int i = 0; i < securePm.publicVms.size(); i++) {
+//				//migrate
+//			}
+//			securePm.switchOff(null);
+//		}		
 	}
 	
 	
